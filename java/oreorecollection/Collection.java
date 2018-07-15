725962c2f487afdc52a234ec38a6cb2c9d3654f9
takuya-hasumi <t_hasumi@innovation.co.jp>
2018-07-15 12:49:54 +0900

    feat: Javaでイテレータパターンを実装

---
+++ java/oreorecollection/Collection.java
@@ -0,0 +1,9 @@
+package oreorecollection;
+
+/**
+ * Collection
+ */
+public interface Collection {
+    // 抽象メソッド
+    Object getIterator();
+}
---
+++ java/oreorecollection/DTUCollection.java
@@ -0,0 +1,51 @@
+package oreorecollection;
+
+import oreoreiterator.DTUIterator;
+import dtumember.DTUMember;
+import java.util.ArrayList;
+
+/**
+ * DTUCollection
+ */
+public class DTUCollection implements Collection{
+    public ArrayList<DTUMember> members = new ArrayList<DTUMember>();
+    public int scale = 0;
+
+    /**
+     * フィールドにメンバーオブジェクトを追加
+     * 
+     * @param  DTUMember member 
+     */
+    public void addMember(DTUMember member) {
+        this.members.add(member);
+        this.scale++;
+    }
+
+    /**
+     * 組織規模を返す
+     * 
+     * @return int scale
+     */
+    public int getScale() {
+        return this.scale;
+    }
+
+    /**
+     * 現在の順番のメンバーを返す
+     * 
+     * @return DTUMember
+     */
+    public DTUMember getMember(int index) {
+        return this.members.get(index);
+    }
+
+    /**
+     * DTUIteratorを返す
+     * 
+     * @return DTUIterator
+     */
+    public DTUIterator getIterator()
+    {
+        return new DTUIterator(this);
+    }
+}
